db255b2359f41900752acf1d09bf458c8e7d6c48
NarDd <1601037@SIT.Singaporetech.edu.sg>
2018-01-25 15:01:41 +0800

    version 2` s Please enter the commit message for your changes. Lines starting

--- www/cordova-plugin-altbeacon-marking.js
+++ www/cordova-plugin-altbeacon-marking.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-var pluginName = "cordova-plugin-altbeacon";
+var pluginName = "cordova-plugin-altbeacon-marking";
 
 exports.enabled = function(success, error) {
     exec(success, error, pluginName, "enabled", []);
